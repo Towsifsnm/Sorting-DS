d1ec7cfc96e930a88b458e57db10d94bda908ede
Towsif Mahabub <147875322+Towsifsnm@users.noreply.github.com>
2024-05-27 23:03:20 +0600

    Create large number.cpp

---
+++ large number.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+
+int findLargestNumber(int arr[], int size) {
+    int largest = arr[0];
+    for (int i = 1; i < size; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+    return largest;
+}
+
+int main() {
+    int n;
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter " << n << " elements:" << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int largest = findLargestNumber(arr, n);
+    cout << "The largest number in the array is: " << largest << endl;
+
+    return 0;
+}
+
